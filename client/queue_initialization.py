e33c38d66693ac301be3c50700d666703e9b23ea
Pranesh <pranesh25joshi@gmail.com>
2025-12-11 11:36:56 +0530

    Remove outdated comment regarding Upstash Redis in queue initialization

--- client/queue_initialization.py
+++ client/queue_initialization.py
@@ -5,10 +5,9 @@ import os
 
 load_dotenv()
 
-# For Upstash Redis TCP connection
 REDIS_HOST = os.getenv("REDIS_HOST", "bursting-piglet-36655.upstash.io")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
-REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")  # The password from redis://default:PASSWORD@host:port
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
 
 redis_client = Redis(
     host=REDIS_HOST,
